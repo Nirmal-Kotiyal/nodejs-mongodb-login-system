33d07f203282fa8ded0a30d812b435602e96ef4c
Nirmal-Kotiyal <nirmal.kotiyal@gmail.com>
2020-01-17 13:36:21 +0530

    Version 1.1

--- server.js
+++ server.js
@@ -8,17 +8,17 @@ const SignIn = require('./routes/SignIn');
 const verify = require('./routes/verify')
 const Logout = require('./routes/Logout')
 
-mongoose.connect(db_config,{ useNewUrlParser: true,useUnifiedTopology: true})
-.then(console.log("Connected to the mongodb"))
-.catch(err=>console.error(err));
+
 
 const app =express()
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use(bodyParser.json())
+mongoose.connect(db_config,{ useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex: true})
+.then(console.log("Connected to the mongodb"))
+.catch(err=>console.error(err));
 
 app.use('/signup',SignUp);
 app.use('/signin',SignIn);
@@ -27,14 +27,18 @@ app.use('/logout',Logout);
 
 
 
-if(process.env.NODE_ENV === 'production') {  app.use(express.static('client/build'));
- app.get('*', (req, res) => {    res.sendfile(path.resolve(__dirname,'client/build/index.html'));  })}
-
-
-app.listen(PORT,(err,res)=>{
-    if(err) console.error(err);
-    console.log(`server is started at port${PORT} `);
-})
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+  
+  const port = process.env.PORT || 5000;
+  
+  app.listen(port, () => console.log(`Server started on port ${port}`));
 
 
 
@@ -55,7 +59,3 @@ app.listen(PORT,(err,res)=>{
 //         'img-src': [csp.SELF],
 //     }
 // }));
-
-
-
-module.exports = app;
