88865f872adc4f0dc1733bcddb55ff0ff0702632
Nirmal-Kotiyal <nirmal.kotiyal@gmail.com>
2020-01-17 14:03:38 +0530

    version 2

--- server.js
+++ server.js
@@ -7,55 +7,55 @@ const SignUp = require('./routes/SignUp');
 const SignIn = require('./routes/SignIn');
 const verify = require('./routes/verify')
 const Logout = require('./routes/Logout')
+const csp = require('express-csp-header');
 
-
+mongoose.connect(db_config,{ useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex:true})
+.then(console.log("Connected to the mongodb"))
+.catch(err=>console.error(err));
 
 const app =express()
 
-app.use(express.json());
-
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(db_config,{ useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex: true})
-.then(console.log("Connected to the mongodb"))
-.catch(err=>console.error(err));
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+if(process.env.NODE_ENV === 'production') {  app.use(express.static(path.join(__dirname, 'client/build')));
+ app.get('*', (req, res) => {    res.sendfile(path.join(path.dirname(__filename).split(path.sep).pop()= 'client/build/index.html'));  })}
+else{
+    app.get('*', (req, res) => {  res.sendFile(path.join(path.dirname(__filename).split(path.sep).pop()+'/client/public/index.html'));})
+}
+
+
+app.listen(PORT,(err,res)=>{
+    if(err) console.error(err);
+    console.log(`server is started at port${PORT} `);
+})
+
+app.use(express.static(path.join(path.dirname(__filename).split(path.sep).pop(),'client/build')));
+
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+     res.header("Access-Control-Allow-Headers",
+     'Origin,X-Requested-With,Content-Type,Accept,Authorization')
+     if(req.method==='OPTIONS'){
+         res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+         return res.status(200).json({});
+    }
+    res.header("Access-Control-Allow-Credentials","true");
+    next();
+});
+app.use(csp({
+    policies: {
+        'default-src': [csp.NONE],
+        'img-src': [csp.SELF],
+    }
+}));
 
 app.use('/signup',SignUp);
 app.use('/signin',SignIn);
 app.use('/verify',verify);
 app.use('/logout',Logout);
 
-
-
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
-  
-  const port = process.env.PORT || 5000;
-  
-  app.listen(port, () => console.log(`Server started on port ${port}`));
-
-
-
-// app.use((req,res,next)=>{
-//     res.header('Access-Control-Allow-Origin','*');
-//      res.header("Access-Control-Allow-Headers",
-//      'Origin,X-Requested-With,Content-Type,Accept,Authorization')
-//      if(req.method==='OPTIONS'){
-//          res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
-//          return res.status(200).json({});
-//     }
-//     res.header("Access-Control-Allow-Credentials","true");
-//     next();
-// });
-// app.use(csp({
-//     policies: {
-//         'default-src': [csp.NONE],
-//         'img-src': [csp.SELF],
-//     }
-// }));
+module.exports = app;
